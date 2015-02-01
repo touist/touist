2e5c5909ac7a29c3129483c40ac2534188e53111
olzd <olzd@users.noreply.github.com>
2015-02-01 23:26:38 +0100

    add tokens

---
+++ compiler/src/tokens.ml
@@ -0,0 +1,60 @@
+type token =
+  (* boolean *)
+  | True
+  | False
+  | Not
+  | And
+  | Or
+  | Equal
+  | NotEqual
+  | LT
+  | GT
+  | LE
+  | GE
+  | Empty
+  | In
+  | Subset
+  (* numeric *)
+  | Plus
+  | Minus
+  | Times
+  | Divide
+  | Modulo
+  | Cardinal
+  | IntPart
+  | Sqrt
+  | Int of int
+  | Float of float
+  (* set *)
+  | Union
+  | Intersect
+  | Diff
+  | If
+  | Then
+  | Else
+  | Upperset
+  | Range        (* 1..9          *)
+  | Dot          (* s.(10)        *)
+  | Set          (* s(?a; ?b; ?c) *)
+  (* clause *)
+  | Top
+  | Bottom
+  | Clause
+  | Negation
+  | LogicalAnd
+  | LogicalOr
+  | Arrow
+  | BigAnd
+  | BigOr
+  (* other *)
+  | Begin
+  | End
+  | Formula
+  | Sets
+  | With
+  | LeftPar
+  | RightPar
+  | Affect
+  | Var of string
+  | Const of string
+  | Eof
