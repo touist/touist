2c6b4440fa19d972168e4383b987f0cc09177c28
Maël Valais <mael65@gmail.com>
2015-02-18 21:09:38 +0100

    Ajout de commentaires de la classe Gestionnaire

--- ui/solution/Gestionnaire.java
+++ ui/solution/Gestionnaire.java
@@ -10,19 +10,30 @@ import translation.ResultatOcaml;
 import translation.Traducteur;
 
 /**
- *
- * @author Skander
+ * Une instance Gestionnaire est le "lanceur" de haut niveau
+ * de la traduction puis de la résolution.
+ * @author Maël
  */
 public class Gestionnaire {
     Traducteur traducteur;
     Models models;
     ResultatOcaml ocaml;
     SolverSAT solverSAT;
-    
+
+    /**
+     * Traduit la base de clauses en utilisant Traducteur
+     * et en produisant ResultatOcaml
+     * @param clauses
+     */
     public void preparation(BaseDeClauses clauses) {
-        
+
     }
-    
+
+    /**
+     * Lance une demande de résolution au solveur
+     * @return le modèle suivant, NULL si non consistant
+     * (insatisfiable)
+     */
     public Model computeModel() {
         return new Model();
     }
