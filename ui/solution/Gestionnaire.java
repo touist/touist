9aec2f74b1da9df4b114bbe112f6b6bb0b3dd36c
Maël Valais <mael65@gmail.com>
2015-02-21 19:33:24 +0100

    Added comments. Translated the whole thing into english.

--- ui/solution/Gestionnaire.java
+++ ui/solution/Gestionnaire.java
@@ -10,8 +10,8 @@ import translation.ResultatOcaml;
 import translation.Traducteur;
 
 /**
- * Une instance Gestionnaire est le "lanceur" de haut niveau
- * de la traduction puis de la résolution par miniSAT.
+ * This class is the high-level launcher for translation
+ * into DIMACS and then solving using the external solveur.
  * @author Maël
  */
 public class Gestionnaire {
@@ -21,19 +21,21 @@ public class Gestionnaire {
     SolverSAT solverSAT;
 
     /**
-     * Traduit la base de clauses en utilisant Traducteur
-     * et en produisant ResultatOcaml
+     * Translates the ClauseBase using the Traducteur
+     * and producing a ResultatOcaml object.
      * @param clauses
      */
     public void preparation(BaseDeClauses clauses) {
+	// 1. Save the ClauseBase into a temporary .bigand file
+	clauses.saveToFile("clauses");
+	// 2. Call the translator (bigand to dimacs)
 	traducteur = new Traducteur();
-	traducteur.appelTraducteurOcaml();
+	traducteur.appelTraducteurOcaml(); // XXX How does it work?
     }
 
     /**
-     * Lance une demande de résolution au solveur
-     * @return le modèle suivant, NULL si non consistant
-     * (insatisfiable)
+     * Asks the external solver if this DIMACS file has models
+     * @return the next model, NULL if no model exists
      */
     public Model computeModel() {
         return new Model();
