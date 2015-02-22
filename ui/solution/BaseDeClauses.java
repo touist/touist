4de1a847a2567e84c1d3ce2a204531f4b585f97b
cup2of2tea <comtealexis@free.fr>
2015-02-22 15:11:00 +0100

    Added sets gestion. Light refactoring for upload method. Completed save method.

--- ui/solution/BaseDeClauses.java
+++ ui/solution/BaseDeClauses.java
@@ -6,10 +6,13 @@
 package solution;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -27,12 +30,24 @@ class FormatException extends Exception {
 
 public class BaseDeClauses {
     private List<String> formules;
-
+    private List<String> sets;
+    
+    private static final String errorBeginFormula = "Syntax error at line %d" 
+                                       + ". Formula begin section already"
+                                       + " declared.";
+    private static final String errorEndFormula =  "Syntax error at line %d"
+                                       + ". No formula section is opened.";
+    private static final String errorBeginSet = 
+                                errorBeginFormula.replace("Formula","Set");
+    private static final String errorEndSet = 
+                                errorEndFormula.replace("formula","set");
+    
     /**
      * Construct a set of clauses
      */
     public BaseDeClauses() {
         formules = new ArrayList<>();
+        sets = new ArrayList<>();
     }
 
     /**
@@ -43,6 +58,13 @@ public class BaseDeClauses {
         return formules;
     }
 
+    /**
+     * @return the list of sets
+     */
+    public List<String> getSets() {
+        return sets;
+    }
+    
 
     /**
      * Import the list of formules from a file
@@ -59,6 +81,7 @@ public class BaseDeClauses {
         String line;
 
         boolean formulaSection = false;
+        boolean setSection = false;
         int indexLine = 0;
 
         while((line = in.readLine()) != null) {
@@ -71,37 +94,74 @@ public class BaseDeClauses {
                 words[1] = words[1].toLowerCase();
 
 
-
-                if(words.equals(new String[]{"begins","sets"})) {
+                if(Arrays.equals(words,new String[]{"begins","formula"})) {
                     if(formulaSection == true) {
-                        String message = "Syntax error at line " + indexLine
-                                       + ". Formula begin section already"
-                                       + " declared.";
+                        String message = String.format(errorBeginFormula,indexLine);
                         throw new FormatException(message);
                     }
                     else {
                         formulaSection = true;
                     }
                 }
-                else if(words.equals(new String[]{"ends","sets"})) {
+                else if(Arrays.equals(words,new String[]{"ends","formula"})) {
                     if(formulaSection == false) {
-                        String message = "Syntax error at line " + indexLine
-                                       + ". No formula section is opened.";
+                        String message = String.format(errorEndFormula,indexLine);
                         throw new FormatException(message);
                     }
                     else {
                         formulaSection = false;
                     }
                 }
+                else if(Arrays.equals(words,new String[]{"begins","sets"})) {
+                    if(setSection == true) {
+                        String message = String.format(errorBeginSet,indexLine);
+                        throw new FormatException(message);
+                    }
+                    else {
+                        setSection = true;
+                    }
+                }
+                else if(Arrays.equals(words,new String[]{"ends","sets"})) {
+                    if(setSection == false) {
+                        String message = String.format(errorEndSet,indexLine); 
+                        throw new FormatException(message);
+                    }
+                    else {
+                        setSection = false;
+                    }
+                }
+                
             }
             else if(formulaSection) {
                 addFormule(line);
             }
+            else if(setSection) {
+                addSet(line);
+            }
         }
     }
 
-    public void saveToFile(String path) {
-	// TODO
+    public void saveToFile(String path) throws IOException {
+        int sizeBuffer = 8192;
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path), sizeBuffer);
+        
+	if(!sets.isEmpty()) {
+            writer.write("begins sets\n");
+            for(String set:sets) {
+                writer.write(set+"\n");
+            }
+            writer.write("ends sets\n");
+        }
+        if(!formules.isEmpty()) {
+            writer.write("begins formules\n");
+            for(String formule:formules) {
+                writer.write(formule+"\n");
+            }
+            writer.write("ends sets\n");
+        }
+        
+        writer.flush();
+        writer.close();
     }
 
     /**
@@ -111,6 +171,15 @@ public class BaseDeClauses {
 
     public void addFormule(String formule) {
         formules.add(formule);
+    }
+    
+    /**
+     * Add a set to the current list of sets
+     * @param A set
+     */
+    
+    public void addSet(String set) {
+        sets.add(set);
     }
 
 
@@ -122,6 +191,18 @@ public class BaseDeClauses {
     public String removeFormule(int index) {
         return formules.remove(index);
     }
-
+    
+    /**
+     * Remove a set at a given index
+     * @param index
+     * @return the set removed
+     */
+    public String removeSet(int index) {
+        return sets.remove(index);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("essai");
+    }
 
 }
