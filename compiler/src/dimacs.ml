8c84bd4954e6bdd3ca796ba3c3cea9aa98274005
olzd <olzd@users.noreply.github.com>
2015-03-06 19:37:37 +0100

    add utility function to turn a hashtable into a string

--- compiler/src/dimacs.ml
+++ compiler/src/dimacs.ml
@@ -1,15 +1,16 @@
 open Ast
 
 let to_dimacs prop =
-  let table = Hashtbl.create 10
-  and num_sym = ref 1 in
+  let table    = Hashtbl.create 10
+  and num_sym  = ref 1
+  and nbclause = ref 0 in
   let rec go acc = function
     | Top                     -> failwith "clause is always true"
     | Bottom                  -> failwith "clause is always false"
     | Term   (x, None)        -> acc ^ string_of_int (gensym x)
     | Term   (x, _)           -> failwith ("unevaluated term: " ^ x)
     | Not    (Term (x, None)) -> acc ^ string_of_int (- (gensym x))
-    | And    (x, y)           -> (go acc x) ^ " 0\n" ^ (go acc y)
+    | And    (x, y)           -> incr nbclause; (go acc x) ^ " 0\n" ^ (go acc y)
     | Or     (x, y)           -> (go acc x) ^ " " ^ (go acc y)
     | _ -> failwith "non CNF clause"
   and gensym x =
@@ -17,17 +18,15 @@ let to_dimacs prop =
     with Not_found ->
       let n = !num_sym in
       Hashtbl.add table x n; incr num_sym; n
-  in (go "" prop) ^ " 0", table
+  in
+  let str = (go "" prop) ^ " 0\n" in
+  let header =
+    "c CNF format file\np cnf " ^ string_of_int (Hashtbl.length table)
+                                ^ " "
+                                ^ string_of_int (!nbclause+1)
+                                ^ "\n"
+  in
+  header ^ str, table
 
-(*
-let test () =
-  let p = Implies (And (Term ("a", None),
-                        Term ("b", None)),
-                   Implies (Or (And (Not (Term ("a", None)), Term ("c", None)),
-                                     Term ("c", None)),
-                                Not (Term ("a", None)))) in
-  let str, table = Cnf.to_cnf p |> to_dimacs in
-  print_string str
-
-let () = test ()
-*)
+let string_of_table table =
+  Hashtbl.fold (fun k v acc -> acc ^ k ^ " " ^ (string_of_int v) ^ "\n") table ""
