4c4c9f47cddab4ccb29f85734d3f06226cb4e4d1
olzd <olzd@users.noreply.github.com>
2015-03-12 18:36:27 +0100

    default output files are now created in the current directory

--- compiler/src/touistc.ml
+++ compiler/src/touistc.ml
@@ -28,6 +28,7 @@ Then you can open the FilePath module:
   open FilePath;;
 *)
 
+
 type mode = SMTLIB2 | SAT_DIMACS
 type error =
   | OK
@@ -46,7 +47,7 @@ let output_table_file_path = ref ""
 let output_file_basename = ref ""
 
 (* Used to write the "str" string into the "filename" file *)
-let write_to_file (filename:bytes) (str:string) =
+let write_to_file (filename:string) (str:string) =
   let out = open_out filename in
   try
     Printf.fprintf out "%s" str;
@@ -55,51 +56,47 @@ let write_to_file (filename:bytes) (str:string) =
 
 (* Used when no outputFilePath is given: builds an arbitrary
 outputFilePath name using the inputFilePath name *)
-let defaultOutput (inputFilePath:bytes) (m:mode) : string =
-  let inputBase = FilePath.basename inputFilePath
-  in let inputDir = FilePath.dirname inputFilePath
-  in let outputBase = match m with
+let defaultOutput (inputFilePath:string) (m:mode) : string =
+  let inputBase = FilePath.basename inputFilePath in
+  match m with
   | SAT_DIMACS -> FilePath.replace_extension inputBase "cnf"
   | SMTLIB2 -> FilePath.replace_extension inputBase "smt2"
-  in FilePath.concat inputDir outputBase
+  (*in FilePath.concat inputDir outputBase*)
 
 (* Used when no outputFilePath is given: builds an arbitrary
 outputFilePath name using the inputFilePath name *)
-let defaultOutputTable (inputFilePath:bytes) : string =
-  let inputBase = (FilePath.basename inputFilePath)
-  in let inputBaseNoExt = (FilePath.chop_extension inputBase)
-  in let inputDir = (FilePath.dirname inputFilePath)
-  in let outputTableBase = ("." ^ inputBaseNoExt ^ "_table")
-  in (FilePath.concat inputDir outputTableBase)
+let defaultOutputTable (inputFilePath:string) : string =
+  let inputBase = (FilePath.basename inputFilePath) in
+  let inputBaseNoExt = (FilePath.chop_extension inputBase) in
+  "." ^ inputBaseNoExt ^ "_table"
 
 (* Used in Arg.parse when a parameter without any preceeding -flag (-f, -x...)
 Here, this kind of parameter is considered as an inputFilePath *)
-let argIsInputFilePath (inputFilePath:bytes) : unit =
+let argIsInputFilePath (inputFilePath:string) : unit =
   input_file_path := inputFilePath
 
 let translateToSATDIMACS
-  (inputFilePath:bytes)
-  (outputFilePath:bytes)
-  (outputTableFilePath:bytes)
+  (inputFilePath:string)
+  (outputFilePath:string)
+  (outputTableFilePath:string)
   : unit =
   let exp =
-    Eval.eval (Parser.prog Lexer.lexer (Lexing.from_channel
-      (open_in inputFilePath))
-    )
-  in let c,t = Cnf.to_cnf exp |> Dimacs.to_dimacs
+    Eval.eval (Parser.prog Lexer.lexer (Lexing.from_channel (open_in inputFilePath)))
   in
-    write_to_file outputFilePath c;
-    write_to_file outputTableFilePath (Dimacs.string_of_table t)
+  let c,t = Cnf.to_cnf exp |> Dimacs.to_dimacs in
+  write_to_file outputFilePath c;
+  write_to_file outputTableFilePath (Dimacs.string_of_table t)
 
 (* The main program *)
 let _ =
-  let cmd = (FilePath.basename Sys.argv.(0)) (* ./touistl exec. name *)
-  in let argspecs = (* This list enumerates the different flags (-x,-f...)*)
+  let cmd = (FilePath.basename Sys.argv.(0)) in (* ./touistl exec. name *)
+  let argspecs = (* This list enumerates the different flags (-x,-f...)*)
   [ (* "-flag", Arg.toSomething (ref var), "Usage for this flag"*)
     ("-o", Arg.Set_string (output_file_path), "The translated file");
     ("-table", Arg.Set_string (output_table_file_path), "The literals table table (for SAT_DIMACS)")
   ]
-  in let usage = "TouistL compiles files from the TouIST Language "
+  in
+  let usage = "TouistL compiles files from the TouIST Language "
     ^"to SAT-DIMACS/SMT-LIB2 \n"
     ^"Usage: "^cmd^" [-o translatedFile] [-table tableFile] file \n"
     ^"Note: if either tableFile or translatedFile is missing, \n"
@@ -113,25 +110,30 @@ let _ =
   (* Step 2: we see if we got every parameter we need *)
   if ((String.length !input_file_path) == 0)(* NOTE: !var is like *var in C *)
   then (
-    print_endline (cmd^": you must give an input file path");
+    print_endline (cmd^": you must give an input file");
     exit (get_code ARGUMENTS_ERROR)
     );
 
   if ((String.length !output_file_path) == 0)
-  then (
+  then
     output_file_path := (defaultOutput !input_file_path SAT_DIMACS);
-    print_endline
-      (cmd ^ ": no output path given ; will be stored in "^ !output_file_path);
-    );
 
   if ((String.length !output_table_file_path) == 0)
-  then (
+  then
     output_table_file_path := (defaultOutputTable !input_file_path);
-    print_endline
-      (cmd ^ ": no output table file path given ; will be stored in "^ !output_table_file_path)
-    );
 
   (* Step 3: translation *)
   translateToSATDIMACS !input_file_path !output_file_path !output_table_file_path;
 
   exit (get_code OK)
+
+(* Quick testing main function
+ * let _ =
+  let input_file = FilePath.basename Sys.argv.(1) in
+  let out_file = FilePath.replace_extension input_file "cnf" in
+  let table_file = "." ^ (FilePath.chop_extension input_file) ^ "_table" in
+  let exp = Eval.eval (Parser.prog Lexer.lexer (Lexing.from_channel (open_in Sys.argv.(1)))) in
+  let c,t = Cnf.to_cnf exp |> Dimacs.to_dimacs in
+  write_to_file out_file c;
+  write_to_file table_file (Dimacs.string_of_table t)
+*)
