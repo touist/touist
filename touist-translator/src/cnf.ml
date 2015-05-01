3f4e1107e6845f1eac26a0e2a19888b7c64f4a73
olzd <olzd@users.noreply.github.com>
2015-05-01 15:21:16 +0200

    update the method to transform to CNF
    We reduce the number of generated clauses (especially in the case of disjunctions).
    We went from exponential to linear in term of the number of clauses in the generated formula.
    See http://en.wikipedia.org/wiki/Conjunctive_normal_form.

--- touist-translator/src/cnf.ml
+++ touist-translator/src/cnf.ml
@@ -1,99 +1,46 @@
 open Syntax
+open Pprint
 
-let to_cnf p =
-  let rec remove_impl = function
-    | Top | Bottom | Term _ as x -> x
-    | CNot     x      -> CNot (remove_impl x)
-    | CAnd     (x, y) -> CAnd (remove_impl x, remove_impl y)
-    | COr      (x, y) -> COr (remove_impl x, remove_impl y)
-    | CXor     (x, y) -> CXor (remove_impl x, remove_impl y)
-    | CImplies (x, y) -> COr (CNot (remove_impl x), remove_impl y)
-    | CEquiv   (x, y) -> CNot (CXor (remove_impl x, remove_impl y))
-    | Bigand _ | Bigor _ | CIf _ | CVar _ ->
-        failwith "all bigor, bigand, if and var should have been eliminated"
-  in
-  let rec remove_xor = function
-    | Top | Bottom | Term _ as x -> x
-    | CNot  x              -> CNot (remove_xor x)
-    | CAnd  (x, y)         -> CAnd (remove_xor x, remove_xor y)
-    | COr   (x, y)         -> COr (remove_xor x, remove_xor y)
-    | CXor  (x, y)         -> CAnd (COr (remove_xor x, remove_xor y), CNot (CAnd (remove_xor x, remove_xor y)))
-    | CImplies _ | CEquiv _ -> failwith "there shouldn't be any implies/equiv left"
-    | Bigand _ | Bigor _ | CIf _ | CVar _  ->
-        failwith "all bigor, bigand, if and var should have been eliminated"
-  in
-  let rec push_neg_in = function
-    | Top | Bottom | Term _ as x -> x
-    | CNot  Top            -> Bottom
-    | CNot  Bottom         -> Top
-    | CNot  (CNot x)       -> push_neg_in x
-    | CNot  (CAnd (x, y))  -> COr (push_neg_in (CNot x), push_neg_in (CNot y))
-    | CNot  (COr (x, y))   -> CAnd (push_neg_in (CNot x), push_neg_in (CNot y))
-    | CNot  x              -> CNot (push_neg_in x)
-    | CAnd  (x, y)         -> CAnd (push_neg_in x, push_neg_in y)
-    | COr   (x, y)         -> COr (push_neg_in x, push_neg_in y)
-    | CXor     _           -> failwith "there shouldn't be any xors left"
-    | CImplies _ | CEquiv _ -> failwith "there shouldn't be any implies/equiv left"
-    | Bigand _ | Bigor _ | CIf _ | CVar _  ->
-        failwith "all bigor, bigand, if and var should have been eliminated"
-  in
-  let rec push_disj_in = function
-    | Top | Bottom | Term _ as x -> x
-    | CNot  x               -> CNot (push_disj_in x)
-    | COr (x, y)            -> dist (push_disj_in x) (push_disj_in y)
-    | CAnd (x, y)           -> CAnd (push_disj_in x, push_disj_in y)
-    | CXor     _            -> failwith "there shouldn't be any xors left"
-    | CImplies _ | CEquiv _ -> failwith "there shouldn't be any implies left"
-    | Bigand _ | Bigor _ | CIf _ | CVar _ ->
-        failwith "all bigor, bigand, if and var should have been eliminated"
-  and dist x y =
-    match x, y with
-    | CAnd (x, y), z -> CAnd (push_disj_in (COr (x, z)), push_disj_in (COr (y, z)))
-    | x, CAnd (y, z) -> CAnd (push_disj_in (COr (x, y)), push_disj_in (COr (x, z)))
-    | x, y           -> COr (push_disj_in x, push_disj_in y)
-  and simplify = function
-    (*| CAnd (x, Top) -> simplify x
-    | CAnd (Top, x) -> simplify x
-    | CAnd (x, Bottom) -> Bottom
-    | CAnd (Bottom, x) -> Bottom
-    | CAnd (CNot (Term (x, None)), Term (y, None)) as p -> if x = y then Bottom else p
-    | CAnd (Term (x, None), CNot (Term (y, None))) as p -> if x = y then Bottom else p
-    | CAnd (x, y) -> if x = y then simplify x else CAnd (simplify x, simplify y)
-    | COr (x, Top) -> Top
-    | COr (Top, x) -> Top
-    | COr (x, Bottom) -> simplify x
-    | COr (Bottom, x) -> simplify x
-    | COr (CNot (Term (x, None)), Term (y, None)) as p -> if x = y then Top else p
-    | COr (Term (x, None), CNot (Term (y, None))) as p -> if x = y then Top else p
-    | COr (COr (Term (x, None), y), CNot (Term (z, None))) as p -> if x = z then simplify y else p
-    | COr (COr (x, Term (y, None)), CNot (Term (z, None))) as p -> if y = z then simplify x else p
-    | COr (COr (CNot (Term (x, None)), y), Term (z, None)) as p -> if x = z then simplify y else p
-    | COr (COr (x, CNot (Term (y, None))), Term (z, None)) as p -> if y = z then simplify x else p
-    | COr (COr (Term (x, None), CNot (Term (y, None))), z) as p -> if x = y then simplify z else p
-    | COr (COr (CNot (Term (x, None)), Term (y, None)), z) as p -> if x = y then simplify z else p
-    | COr (x, y) -> if x = y then simplify x else COr (simplify x, simplify y)
-    | x -> x*)
-    | Top -> Top
-    | Bottom -> Bottom
-    | CAnd (x,y) -> simplify_and x y
-    | COr (x,y) -> simplify_or x y
-    | CNot x -> simplify_not (CNot x)
-    | x -> x
-  and simplify_and x y =
-    match simplify x, simplify y with
-    | Top,Top             -> Top
-    | x,Top | Top,x       -> simplify x
-    | _,Bottom | Bottom,_ -> Bottom
-    | x',y' -> if x' = y' then x' else (CAnd (simplify x',simplify y'))
-  and simplify_or x y =
-    match simplify x, simplify y with
-    | Bottom,Bottom       -> Bottom
-    | x,Bottom | Bottom,x -> simplify x
-    | Top,_ | _,Top       -> Top
-    | x',y' -> if x' = y' then x' else (COr (simplify x', simplify y'))
-  and simplify_not = function
-    | CNot Top    -> Bottom
-    | CNot Bottom -> Top
-    | CNot x      -> (CNot (simplify x))
-  in
-  remove_impl p |> remove_xor |> push_neg_in |> push_disj_in |> simplify
+let dummy_term_count = ref 0
+let genterm () =
+  incr dummy_term_count; Term ("&" ^ (string_of_int !dummy_term_count), None)
+
+let rec push_lit lit = function
+  | Term x -> COr (lit, Term x)
+  | CNot (Term x) -> COr (lit, CNot (Term x))
+  | CAnd (x,y) -> CAnd (push_lit lit x, push_lit lit y)
+  | COr (x,y) -> COr (lit, COr (x,y))
+  | x -> failwith ("push_lit: unexpected value " ^ (string_of_clause x)) 
+
+let rec to_cnf = function
+  | Top    -> failwith "Clause always true"
+  | Bottom -> failwith "Clause always false"
+  | Term x -> Term x
+  | CAnd (x,y) -> CAnd (to_cnf x, to_cnf y)
+  | CNot x ->
+      begin
+        match x with
+        | Term a -> CNot (Term a)
+        | CNot y -> to_cnf y
+        | COr (x',y') -> CAnd (to_cnf (CNot x'), to_cnf (CNot y'))
+        | _ -> failwith "Failed to transform to CNF"
+      end
+  | COr (x,y) ->
+      begin
+        match x,y with
+        | Term a, Term b               -> COr (Term a, Term b)
+        | CNot (Term a), Term b        -> COr (CNot (Term a), Term b)
+        | CNot (Term a), CNot (Term b) -> COr (CNot (Term a), CNot (Term b))
+        | Term a, CNot (Term b)        -> COr (Term a, CNot (Term b))
+        | Term a, y' | y', Term a -> push_lit (Term a) (to_cnf y')
+        | CNot (Term a), y' | y', CNot (Term a) -> push_lit (CNot (Term a)) (to_cnf y')
+        | _,_ ->
+            let (new1, new2) = (genterm (), genterm ()) in
+            CAnd (COr (new1, new2), CAnd (push_lit (CNot new1) (to_cnf x),
+                                          push_lit (CNot new2) (to_cnf y)))
+      end
+  | CImplies (x,y) -> to_cnf (COr (CNot x, y))
+  | CEquiv (x,y) -> to_cnf (CAnd (CImplies (x,y), CImplies (y,x)))
+  | CXor (x,y) -> to_cnf (CAnd (COr (x,y), COr (CNot x, CNot y)))
+  | _ -> failwith "Failed to transform to CNF"
+
