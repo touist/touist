6605e0362b410b5b256458c8a84813bfd5bb7f69
maelvalais <mael.valais@univ-tlse3.fr>
2015-05-02 14:18:21 +0200

    Added properties class #90

---
+++ touist-gui/src/touist/TouistProperties.java
@@ -0,0 +1,46 @@
+package touist;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class TouistProperties {
+	private static Map<String,String> properties = new HashMap<String,String>();
+	
+	/**
+     * Loads the version.properties and fill the different
+     * variables VERSION, BUILD_TIMESTAMP...
+     */
+    private Map<String, String> getPropertiesMap() {
+    	if(properties != null) {
+    		return properties;
+    	}
+		Properties prop = new Properties();
+		String propFileName = "version.properties";
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+		try {
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		properties.put("minimum_java_version", prop.getProperty("minimum_java_version"));
+		properties.put("version", prop.getProperty("version"));
+		properties.put("timestamp", prop.getProperty("timestamp"));
+		return properties;
+    }
+    
+	public String getProperty(String property) {
+		return getPropertiesMap().get(property);
+	}
+}
