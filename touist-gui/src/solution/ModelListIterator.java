3db3f3e75f417f41a6661ab64be996fe257f9856
cup2of2tea <comtealexis@free.fr>
2015-05-03 16:43:53 +0200

    #88 Improved performances

--- touist-gui/src/solution/ModelListIterator.java
+++ touist-gui/src/solution/ModelListIterator.java
@@ -28,10 +28,13 @@ import java.util.List;
 import java.util.ListIterator;
 
 import entity.Model;
+import entity.LexicographicalTree;
+import java.util.Set;
 
 public class ModelListIterator implements ListIterator<Model> {
 	private Solver solverInterface;
 	private List<Model> models;
+        private LexicographicalTree alreadyPresent;
 	private int currentPosition;
 
 	/**
@@ -44,6 +47,7 @@ public class ModelListIterator implements ListIterator<Model> {
 		this.models = models;
 		this.solverInterface = solverInterface;
 		currentPosition = -1;
+                this.alreadyPresent = new LexicographicalTree();
 	}
 
 	@Override
@@ -61,7 +65,7 @@ public class ModelListIterator implements ListIterator<Model> {
 			}
 			
 			// Added for filtering '&45' literals
-			if(models.contains(nextModel)) {
+			if(nextModel != null && alreadyPresent.contains(nextModel)) {
 				return hasNext();
 			}
 
@@ -72,6 +76,7 @@ public class ModelListIterator implements ListIterator<Model> {
 			} else {
 				hasNext = true;
 				models.add(nextModel);
+                                alreadyPresent.add(nextModel);
 			}
 		} else { // Models have already been retrieved (e.g. because of
 					// previous())
