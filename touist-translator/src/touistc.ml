fb0b6c679370e7f3bba6643acaed5f7457da10a6
Maël Valais <mael.valais@gmail.com>
2016-10-25 13:31:43 +0200

    Proper error messages in cnf.ml

--- touist-translator/src/touistc.ml
+++ touist-translator/src/touistc.ml
@@ -135,6 +135,11 @@ let evaluate (ast:Syntax.prog) : Syntax.exp =
     Printf.fprintf stderr "argument error: '%s'\n" msg;
     exit (get_code COMPILE_NO_LINE_NUMBER_ERROR)
 
+let transform_to_cnf (evaluated_ast:Syntax.exp) : Syntax.exp =
+  try Cnf.transform_to_cnf evaluated_ast !debug_cnf with
+  | Cnf.Error msg ->
+    Printf.fprintf stderr "%s\n" msg;
+    exit (get_code COMPILE_NO_LINE_NUMBER_ERROR)
 
 (* [lexer] is an intermediate to the [Lexer.token] function (in lexer.mll);
    - Rationale: the parser only accepts Parser.token; but [Lexer.token] returns
@@ -284,7 +289,7 @@ let () =
 
   (* Step 3: translation *)
   if (!sat_mode) then
-    let cnf = Cnf.transform_to_cnf evaluated_ast !debug_cnf in
+    let cnf = transform_to_cnf evaluated_ast in
     if !solve_sat then
       let instance,table = Dimacs.minisat_of_cnf cnf in
       let models = ref Dimacs.ModelSet.empty in
