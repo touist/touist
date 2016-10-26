6251c5f133e32d06d20c8582c04030b6b5512670
Maël Valais <mael.valais@gmail.com>
2016-10-26 02:20:14 +0200

    lexer.mll: errors now correctly prints '1:2: <msg>'
    Instead of '1:2 <msg>'
    (a colon was missing compared to the parser errors)

--- touist-translator/src/touistc.ml
+++ touist-translator/src/touistc.ml
@@ -117,7 +117,7 @@ let argIsInputFilePath (inputFilePath:string) : unit =
 (* Used by parse_with_error *)
 let print_position outx lexbuf =
   let pos = lexbuf.lex_curr_p in
-  Printf.fprintf outx "%d:%d" pos.pos_lnum (pos.pos_cnum - pos.pos_bol+1)
+  Printf.fprintf outx "%d:%d:" pos.pos_lnum (pos.pos_cnum - pos.pos_bol+1)
 
 (* [evaluate] handles exceptions when calling the evaluation function [Eval.eval].
  * Eval.eval takes an abstract syntaxic tree and check that it is semantically correct,
