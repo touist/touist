657ca6564d7a1f41c589bd3022d6d2edaa805472
Maël Valais <mael.valais@gmail.com>
2017-03-09 18:14:40 +0100

    touistc: added 'overall' test on sudoku, checks that model is right

--- touist-translator/test/test.ml
+++ touist-translator/test/test.ml
@@ -3,19 +3,16 @@ open OUnit2;;
 (* To check that the error has occured curreclty, we only check
    that the place where the error was found is the right one.  *)
 let test_raise (parse:(string->unit)) (loc_expected:string) text =
-  let print_loc loc = Printf.sprintf "'%s'" loc in
-  let exception_is_correct =
-    try let _= parse text in false
-    with Eval.Error (_,loc) -> begin
-      OUnit2.assert_equal 
-        ~msg:"line and column of expected Eval.Error are different"
-        ~printer:print_loc loc_expected (Parse.string_of_loc loc);
-        true
-      end
-  in match exception_is_correct with
-  | true -> ()
-  | false -> raise (OUnit2.assert_failure (
-    "this test should have raised Eval.Error exception with location '"^loc_expected^"'"))
+  try let _= parse text in
+    (Printf.fprintf stdout "%s" "OKç";
+    raise (OUnit2.assert_failure (
+      "this test should have raised Eval.Error exception with location '"^loc_expected^"'")))
+  with 
+    | Eval.Error (msg,loc) -> OUnit2.assert_equal
+        ~msg:("the 'line:column:' of expected and actual exception Eval.Error are different; actual error was:\n"^msg)
+        ~printer:(fun loc -> Printf.sprintf "'%s'" loc)
+        loc_expected (Parse.string_of_loc loc)
+
 
 let sat text = let _= Parse.parse_sat text |> Eval.eval |> Cnf.ast_to_cnf |> Sat.cnf_to_clauses in ()
 let smt text = let _= Parse.parse_smt text |> Eval.eval |> Smt.to_smt2 "QF_IDL" in ()
@@ -41,11 +38,58 @@ let test_smt_raise loc text _ = test_raise smt loc text
        "The other operand is of type 'float':\n    3.",loc))
       (fun _ -> eval "let $i=1: if $i < 3.0 then a else b end"));
 *)
-let loc = (Lexing.dummy_pos,Lexing.dummy_pos)
+
+let read_file (filename:string) : string list =
+  let lines = ref [] in
+  let chan = open_in filename in
+  try
+    while true; do
+      lines := input_line chan :: !lines
+    done; !lines
+  with End_of_file ->
+    close_in chan;
+    List.rev !lines ;;
+
+let open_stream name = 
+  let chan = open_in name 
+  in Stream.from (
+    fun _ -> try Some (input_line chan) 
+             with End_of_file -> None)
+
+
+let check_solution (sorted_solution:string) (stream:char Stream.t) =
+  let rec one_line stream : string =
+    try match Stream.next stream with
+      | '\n' -> ""
+      | c -> (Printf.sprintf "%c" c) ^ one_line stream
+    with Stream.Failure -> "" 
+  in 
+  let lines_from_stream (stream:char Stream.t) : string list =
+    let rec multiple_lines stream = match one_line stream with
+    | "" -> []
+    | line -> line::(multiple_lines stream)
+    in multiple_lines stream
+  in
+  let rec rm_unwanted_lines (l:string list) : string list = match l with
+    | [] -> []
+    | x::xs -> if Str.string_match (Str.regexp "^=") x 0
+               then rm_unwanted_lines xs
+               else x::(rm_unwanted_lines xs)
+  in
+  let expected = read_file sorted_solution in
+  let actual = rm_unwanted_lines (List.sort (compare) (lines_from_stream stream)) in
+  let rec check expected actual = match expected,actual with
+    | [],[] -> ()
+    | [],_ | _,[] -> OUnit2.assert_failure "not the same number of output"
+    | exp::xs, act::ys -> OUnit2.assert_equal ~printer:(fun e -> e) exp act;
+        (*Printf.fprintf stdout "expected: %s actual: %s\n" exp act;*) check xs ys
+  in check expected actual
 
 (* Name the test cases and group them together *)
 let () = 
 run_test_tt_main (
+"touist">:::[
+
 "samples of code that should be correct with -sat and -smt2">:::[ (* 'c' is the testing context *)
   "lower than">::      (test_sat "let $i=1.0: if $i < 3.0 then a else b end");
   "bigand and >">::    (test_sat "bigand $i in [1..5] when $i > 2: p($i) end");
@@ -59,18 +103,18 @@ run_test_tt_main (
   "affect between">::  (test_sat "$a = a f($a,$b) $b = b");
   
   "var-tuple is prop">::(test_sat "$a=p p($a)");
-]);
+];
 
-run_test_tt_main (
 "samples of code that should raise errors in [Eval.eval]">:::[ (* 'c' is the testing context *)
-  "defined vars">::         (test_sat_raise "1:4:" "   $a");
+  "undefined var">::         (test_sat_raise "1:4:" "   $a");
   "bigand: too many vars">::(test_sat_raise "1:8:" "bigand $i,$j in [1]: p end");
   "bigand: too many sets">::(test_sat_raise "1:8:" "bigand $i in [1],[2]: p end");
   "bigor: too many vars">::(test_sat_raise "1:7:" "bigor $i,$j in [1]: p end");
   "bigor: too many sets">::(test_sat_raise "1:7:" "bigor $i in [1],[2]: p end");
   "condition is bool">::(test_sat_raise "1:23:" "bigand $i in [1] when a: p end");
-]);
-run_test_tt_main (
+  (*"bigand var is not tuple">::(test_sat_raise "1:23:" "bigand $i(p) in [1]: p end");*)
+];
+
 "samples of code that should be correct with -smt2">:::[ (* 'c' is the testing context *)
   "lower than">::      (test_smt "let $i=1.0: if $i < 3.0 then a else b end");
   "bigand and >">::    (test_smt "bigand $i in [1..5] when $i > 2: p($i) end");
@@ -83,5 +127,13 @@ run_test_tt_main (
   
   "affect between">::  (test_smt "a");
   "affect between">::  (test_smt "a > 3");
-]); 
+];
+"real-size tests">:::[
+  "sodoku">:: (fun ctx -> OUnit2.assert_command
+      ~foutput:(check_solution "real-size-tests/sudoku_solution.txt") ~use_stderr:false ~ctxt:ctx
+      "../touistc.byte" ["--solve";"-sat";"real-size-tests/sudoku.touistl"]);
+];
+
+])
+
 ;;
