47b795612977e1ab12106010bf19e60325231662
Maël Valais <mael.valais@gmail.com>
2017-05-26 14:52:00 +0200

    touist: Pprint.string_of_ast can now print utf8 chars (⋁,⋀,∀,∃...)

--- src/lib/pprint.ml
+++ src/lib/pprint.ml
@@ -6,88 +6,108 @@ open Types.Ast
 
 (** [string_of_ast] takes an abstract syntaxic tree.
     @param ast of type [Types.Ast.ast] *)
-let rec string_of_ast ?(show_var=(fun ast -> "")) ?(debug=false) ?(parenthesis=debug) ast =
-  let string_of_ast = string_of_ast ~show_var:show_var ~parenthesis:parenthesis ~debug:debug in
+let rec string_of_ast ?(utf8=false) ?(show_var=(fun ast -> "")) ?(debug=false) ?(parenthesis=debug) ast =
+  let of_ast = string_of_ast ~utf8:utf8 ~show_var:show_var ~parenthesis:parenthesis ~debug:debug in
+  let of_ast_list = string_of_ast_list in
   match ast with
   | Int    x -> string_of_int x
   | Float  x -> string_of_float x
   | Bool   x -> string_of_bool x
+  | Top when utf8 -> "⟙"
   | Top    -> "Top"
+  | Bottom when utf8 -> "⟘"
   | Bottom -> "Bot"
   | Prop x | UnexpProp (x, None)-> x
-  | UnexpProp (x,Some y) -> x ^ "(" ^ (string_of_ast_list "," y) ^ ")"
+  | UnexpProp (x,Some y) -> x ^ "(" ^ of_ast_list "," y ^ ")"
   | Var (x,None)   -> x ^ (show_var ast)
-  | Var (x,Some y) -> x ^ "(" ^ (string_of_ast_list "," y) ^ ")" ^ show_var ast
-  | Set    x -> "[" ^ string_of_ast_list "," (AstSet.elements x) ^ "]"
-  | Set_decl x -> "[" ^ (string_of_ast_list "," x) ^ "]"
-  | Neg x     -> "(- " ^ (string_of_ast x) ^ ")"
-  | Add (x,y) -> "(" ^ (string_of_ast x) ^ " + "   ^ (string_of_ast y) ^ ")"
-  | Sub (x,y) -> "(" ^ (string_of_ast x) ^ " - "   ^ (string_of_ast y) ^ ")"
-  | Mul (x,y) -> "(" ^ (string_of_ast x) ^ " * "   ^ (string_of_ast y) ^ ")"
-  | Div (x,y) -> "(" ^ (string_of_ast x) ^ " / "   ^ (string_of_ast y) ^ ")"
-  | Mod (x,y) -> "(" ^ (string_of_ast x) ^ " mod " ^ (string_of_ast y) ^ ")"
-  | Sqrt     x -> "sqrt("  ^ (string_of_ast x) ^ ")"
-  | To_int   x -> "int("   ^ (string_of_ast x) ^ ")"
-  | Abs   x -> "abs("   ^ (string_of_ast x) ^ ")"
-  | To_float x -> "float(" ^ (string_of_ast x) ^ ")"
-  | Not     x     -> "not " ^ string_of_ast x
-  | And     (x,y) -> "(" ^ (string_of_ast x) ^ " and " ^ (string_of_ast y) ^ ")"
-  | Or      (x,y) -> "(" ^ (string_of_ast x) ^ " or "  ^ (string_of_ast y) ^ ")"
-  | Xor     (x,y) -> "(" ^ (string_of_ast x) ^ " xor " ^ (string_of_ast y) ^ ")"
-  | Implies (x,y) -> "(" ^ (string_of_ast x) ^ " => "  ^ (string_of_ast y) ^ ")"
-  | Equiv   (x,y) -> "(" ^ (string_of_ast x) ^ " <=> " ^ (string_of_ast y) ^ ")"
-  | Equal            (x,y) -> (string_of_ast x) ^ " == " ^ (string_of_ast y)
-  | Not_equal        (x,y) -> (string_of_ast x) ^ " != " ^ (string_of_ast y)
-  | Lesser_than      (x,y) -> (string_of_ast x) ^ " < "  ^ (string_of_ast y)
-  | Lesser_or_equal  (x,y) -> (string_of_ast x) ^ " <= " ^ (string_of_ast y)
-  | Greater_than     (x,y) -> (string_of_ast x) ^ " > "  ^ (string_of_ast y)
-  | Greater_or_equal (x,y) -> (string_of_ast x) ^ " >= " ^ (string_of_ast y)
-  | Union  (x,y) -> "union("  ^ (string_of_ast x) ^ "," ^ (string_of_ast y)
-  | Inter  (x,y) -> "inter("  ^ (string_of_ast x) ^ "," ^ (string_of_ast y)
-  | Diff   (x,y) -> "diff("   ^ (string_of_ast x) ^ "," ^ (string_of_ast y)
-  | Range  (x,y) -> "["       ^ (string_of_ast x) ^ ".." ^ (string_of_ast y) ^ "]"
-  | Subset (x,y) -> "subset(" ^ (string_of_ast x) ^ "," ^ (string_of_ast y)
-  | Powerset x   -> "powerset(" ^ string_of_ast x ^ ")"
-  | In     (x,y) -> (string_of_ast x) ^ " in " ^ (string_of_ast y)
-  | Empty x -> "empty(" ^ (string_of_ast x) ^ ")"
-  | Card  x -> "card("  ^ (string_of_ast x) ^ ")"
+  | Var (x,Some y) -> x ^ "(" ^ (of_ast_list "," y) ^ ")" ^ show_var ast
+  | Set    x -> "[" ^ of_ast_list "," (AstSet.elements x) ^ "]"
+  | Set_decl x -> "[" ^ of_ast_list "," x ^ "]"
+  | Neg x     -> "(- " ^ of_ast x ^ ")"
+  | Add (x,y) -> "(" ^ of_ast x ^ " + "   ^ of_ast y ^ ")"
+  | Sub (x,y) -> "(" ^ of_ast x ^ " - "   ^ of_ast y ^ ")"
+  | Mul (x,y) -> "(" ^ of_ast x ^ " * "   ^ of_ast y ^ ")"
+  | Div (x,y) -> "(" ^ of_ast x ^ " / "   ^ of_ast y ^ ")"
+  | Mod (x,y) -> "(" ^ of_ast x ^ " mod " ^ of_ast y ^ ")"
+  | Sqrt     x -> "sqrt("  ^ of_ast x ^ ")"
+  | To_int   x -> "int("   ^ of_ast x ^ ")"
+  | Abs   x -> "abs("   ^ of_ast x ^ ")"
+  | To_float x -> "float(" ^ of_ast x ^ ")"
+  | Not     x when utf8    -> "¬" ^ of_ast x
+  | Not     x     -> "not " ^ of_ast x
+  | And     (x,y) when utf8 -> "(" ^ of_ast x ^ " ⋀ " ^ of_ast y ^ ")"
+  | And     (x,y) -> "(" ^ of_ast x ^ " and " ^ of_ast y ^ ")"
+  | Or      (x,y) when utf8 -> "(" ^ of_ast x ^ " ⋁ "  ^ of_ast y ^ ")"
+  | Or      (x,y) -> "(" ^ of_ast x ^ " or "  ^ of_ast y ^ ")"
+  | Xor     (x,y) -> "(" ^ of_ast x ^ " xor " ^ of_ast y ^ ")"
+  | Implies (x,y) when utf8 -> "(" ^ of_ast x ^ " ⇒ "  ^ of_ast y ^ ")"
+  | Implies (x,y) -> "(" ^ of_ast x ^ " => "  ^ of_ast y ^ ")"
+  | Equiv   (x,y) when utf8 -> "(" ^ of_ast x ^ " ⇔ " ^ of_ast y ^ ")"
+  | Equiv   (x,y) -> "(" ^ of_ast x ^ " <=> " ^ of_ast y ^ ")"
+  | Equal            (x,y) -> of_ast x ^ " == " ^ of_ast y
+  | Not_equal        (x,y) when utf8  -> of_ast x ^ " ≠ " ^ of_ast y
+  | Not_equal        (x,y) -> of_ast x ^ " != " ^ of_ast y
+  | Lesser_than      (x,y) -> of_ast x ^ " < "  ^ of_ast y
+  | Lesser_or_equal  (x,y) when utf8 -> of_ast x ^ " ≤ " ^ of_ast y
+  | Lesser_or_equal  (x,y) -> of_ast x ^ " <= " ^ of_ast y
+  | Greater_than     (x,y) -> of_ast x ^ " > "  ^ of_ast y
+  | Greater_or_equal (x,y) when utf8 -> of_ast x ^ " ≥ " ^ of_ast y
+  | Greater_or_equal (x,y) -> of_ast x ^ " >= " ^ of_ast y
+  | Union  (x,y) when utf8 -> of_ast x ^ " ∪ " ^ of_ast y
+  | Union  (x,y) -> "union("  ^ of_ast x ^ "," ^ of_ast y
+  | Inter  (x,y) when utf8 -> of_ast x ^ " ∩ " ^ of_ast y
+  | Inter  (x,y) -> "inter("  ^ of_ast x ^ "," ^ of_ast y
+  | Diff   (x,y) when utf8 -> of_ast x ^ "\\" ^ of_ast y
+  | Diff   (x,y) -> "diff("   ^ of_ast x ^ "," ^ of_ast y
+  | Range  (x,y) -> "["       ^ of_ast x ^ ".." ^ of_ast y ^ "]"
+  | Subset (x,y) when utf8 -> of_ast x ^ " ⊆ " ^ of_ast y
+  | Subset (x,y) -> "subset(" ^ of_ast x ^ "," ^ of_ast y
+  | Powerset x   -> "powerset(" ^ of_ast x ^ ")"
+  | In     (x,y) when utf8 -> of_ast x ^ " ∈ " ^ of_ast y
+  | In     (x,y) -> of_ast x ^ " in " ^ of_ast y
+  | Empty x when utf8 -> of_ast x ^ "=∅"
+  | Empty x -> "empty(" ^ of_ast x ^ ")"
+  | Card  x when utf8 -> "|"  ^ of_ast x ^ "|"
+  | Card  x -> "card("  ^ of_ast x ^ ")"
   | If (x,y,z) ->
-      "if " ^ (string_of_ast x)
-      ^ " then\n" ^ (string_of_ast y)
-      ^ "\nelse\n" ^ (string_of_ast z)
+      "if " ^ of_ast x
+      ^ " then\n" ^ of_ast y
+      ^ "\nelse\n" ^ of_ast z
       ^ "\nend\n"
   | Bigand (x,y,None,z) ->
-      "bigand " ^ (string_of_ast_list "," x)
-       ^ " in " ^ (string_of_ast_list "," y)
-       ^ ":\n"  ^ (string_of_ast z)
+      "bigand " ^ (of_ast_list "," x)
+       ^ " in " ^ (of_ast_list "," y)
+       ^ ":\n"  ^ of_ast z
        ^ "\nend\n"
   | Bigand (x,y,Some b,z) ->
-      "bigand " ^ (string_of_ast_list "," x)
-       ^ " in "   ^ (string_of_ast_list "," y)
-       ^ " when " ^ (string_of_ast b)
-       ^ ":\n"    ^ (string_of_ast z)
+      "bigand " ^ (of_ast_list "," x)
+       ^ " in "   ^ (of_ast_list "," y)
+       ^ " when " ^ of_ast b
+       ^ ":\n"    ^ of_ast z
        ^ "\nend\n"
   | Bigor (x,y,None,z) ->
-      "bigor " ^ (string_of_ast_list "," x)
-       ^ " in " ^ (string_of_ast_list "," y)
-       ^ ":\n"  ^ (string_of_ast z)
+      "bigor " ^ (of_ast_list "," x)
+       ^ " in " ^ (of_ast_list "," y)
+       ^ ":\n"  ^ of_ast z
        ^ "\nend\n"
   | Bigor (x,y,Some b,z) ->
-      "bigor " ^ (string_of_ast_list "," x)
-       ^ " in "   ^ (string_of_ast_list "," y)
-       ^ " when " ^ (string_of_ast b)
-       ^ ":\n"    ^ (string_of_ast z)
+      "bigor " ^ (of_ast_list "," x)
+       ^ " in "   ^ (of_ast_list "," y)
+       ^ " when " ^ of_ast b
+       ^ ":\n"    ^ of_ast z
        ^ "\nend\n"
-  | Exact (x,y) -> "exact(" ^ (string_of_ast x) ^ "," ^ (string_of_ast y) ^ ")"
-  | Atmost (x,y) -> "atmost(" ^ (string_of_ast x) ^ "," ^ (string_of_ast y) ^ ")"
-  | Atleast (x,y) -> "atleast(" ^ (string_of_ast x) ^ "," ^ (string_of_ast y) ^ ")"
-  | Let (v,x,c) -> (string_of_ast v) ^ "=" ^ (string_of_ast x) ^ ": " ^ (string_of_ast c)
-  | Affect (v,c) -> (string_of_ast v) ^ "=" ^ (string_of_ast c)
-  | Touist_code (f) -> (string_of_ast_list "\n" f)
-  | Loc (x,l) -> (if debug then "("^(Msgs.string_of_loc l)^")" else "") ^ string_of_ast x
-  | Paren x -> string_of_ast x
-  | Exists (v,f) -> "exact "^(string_of_ast v) ^": "^ (string_of_ast f)
-  | Forall (v,f) -> "forall "^(string_of_ast v) ^": "^ (string_of_ast f)
+  | Exact (x,y) -> "exact(" ^ of_ast x ^ "," ^ of_ast y ^ ")"
+  | Atmost (x,y) -> "atmost(" ^ of_ast x ^ "," ^ of_ast y ^ ")"
+  | Atleast (x,y) -> "atleast(" ^ of_ast x ^ "," ^ of_ast y ^ ")"
+  | Let (v,x,c) -> of_ast v ^ "=" ^ of_ast x ^ ": " ^ of_ast c
+  | Affect (v,c) -> of_ast v ^ "=" ^ of_ast c
+  | Touist_code (f) -> (of_ast_list "\n" f)
+  | Loc (x,l) -> (if debug then "("^ Msgs.string_of_loc l ^")" else "") ^ of_ast x
+  | Paren x -> of_ast x
+  | Exists (v,f) when utf8 -> "∃"^ of_ast v ^"."^ of_ast f
+  | Exists (v,f)             -> "exists "^ of_ast v ^": "^ of_ast f
+  | Forall (v,f) when utf8 -> "∀" ^ of_ast v ^"."^ of_ast f
+  | Forall (v,f)             -> "forall "^ of_ast v ^": "^ of_ast f
 
 and string_of_ast_type = function
   | Int    x               -> "int"
