230b7e53f62f542d7f9ff1268b3527913afaf2a4
Maël Valais <mael.valais@gmail.com>
2017-05-27 20:53:14 +0200

    fix coloring and ending newline for error messages
    also fixed max size to 76

--- src/lib/msgs.ml
+++ src/lib/msgs.ml
@@ -5,7 +5,17 @@
     - either one of the parse/eval/smt/sat function will raise the Fatal exception.
       In this case, after the exception is catched, you can run [print_msgs].
     - or no fatal error has been encoundered; if you want to display the errors,
-      you can run [print_msgs]. *)
+      you can run [print_msgs].
+
+   Rules for good error or warning messages:
+    - a message must always end with a newline
+    - use a new line (\n) only when the line break is mandatory for
+      understanding. The text will be wrapped automatically.
+    - use indentations for pieces of code you want to show: the indentation
+      will be kept when wrapping as long as no \n is encountered.
+      Indented text is colored.
+    - the text in simple quote '...' or backquotes `...` is colored
+*)
 
 open Lexing (* for Lexing.position *)
 
@@ -50,72 +60,72 @@ let loc_placeholders loc chr =
 
 let all_placeholders loc typ with_colors msg = function
   | 'm' -> msg
-  | 't' ->
-    let typ_txt = match typ with Warning -> "warning" | Error -> "error" in
-    let colorstart,colorend = match with_colors,typ with
-      | false, _       -> "",""
-      | true , Warning -> "\x1b[33m\x1b[1m" (* yellow bold *), "\x1b[0m" (* reset *)
-      | true , Error   -> "\x1b[31m\x1b[1m" (* red bold    *), "\x1b[0m" (* reset *)
-    in colorstart ^ typ_txt ^ colorend
+  | 't' -> (match typ with Warning -> "warning" | Error -> "error")
   | c -> loc_placeholders loc c
 
 (** [replace] replaces all '%c' for any character 'c' using the provided
-    function 'placeholder'. Also replaces all '\n' to actual newlines. *)
+    function 'placeholder'. Also replaces all two-characters '\n' to actual
+    one-character newline. *)
 let replace (placeholder : char -> string) text =
   let text = Str.global_replace (Str.regexp "\\\\n") "\n" text in
   let text = Str.global_replace (Str.regexp "\\\\t") "\t" text in
-  let rec replace placeholder text =
+  let rec replace cur_pos =
     try
-      let next_pos = Str.search_forward (Str.regexp "%[a-zA-Z]") text 0 in
-      let expanded = placeholder (String.get text (next_pos+1)) in
-      Str.string_before text next_pos
-        ^ expanded
-        ^ replace placeholder (Str.string_after text (next_pos+2))
-    with Not_found -> ""
-  in replace placeholder text
+      let next_pos = Str.search_forward (Str.regexp "%[a-zA-Z]") text cur_pos in
+      (*Printf.printf "cur=%d next=%d len=%d (%s)\n" cur_pos next_pos (String.length text) text;*)
+      String.sub text cur_pos (next_pos-cur_pos)
+        ^ (String.get text (next_pos+1) |> placeholder)
+        ^ if next_pos+2 <= String.length text-1 then replace (next_pos+2) else ""
+    with Not_found -> String.sub text cur_pos (String.length text - cur_pos)
+  in replace 0
 
 let string_of_loc ?(fmt="%l:%c") (loc:loc) : string =
   replace (loc_placeholders loc) fmt
 
 (** Wraps the text at width. Indendation is kept as long no new line is read.
     If width = 0, do not wrap. *)
-let format_width width text =
-  let rec format prev_indent text =
-    let indent = try Str.search_forward (Str.regexp "[^ ]") text 0 with Not_found -> 0 in
+let format_width color width text =
+  let rec format prev_indent from_pos =
+    let cur_indent = try (Str.search_forward (Str.regexp "[^ ]") text from_pos)-from_pos
+                     with Not_found -> 0 in
     let wrap_pos =
-      let newline =
-        try Str.search_forward (Str.regexp "\n") text 0
-        with Not_found -> String.length text in
-      if newline > width then width else newline
+      let newline_pos =
+        try Str.search_forward (Str.regexp "\n") text from_pos
+        with Not_found -> String.length text
+      in if newline_pos > from_pos+width then from_pos+width else newline_pos
     in
     let rec spaces = function 0 -> "" | x -> " "^ spaces (x-1) in
-    (*Printf.printf "%s\n-> newline=%d,indent=%d,prev_indent=%d,len=%d\n\n" text wrap_pos indent prev_indent (String.length text);*)
     match wrap_pos with
-    | wrap when wrap = String.length text -> spaces prev_indent ^ text
-    | wrap when String.get text wrap = '\n' -> (* wrap at a \n symbol *)
-      spaces prev_indent ^ Str.string_before text (wrap+1) ^ format 0 (Str.string_after text (wrap+1))
-    | wrap -> (* wrap at any point because width is too large *)
-      let last_space = try Str.search_backward (Str.regexp "[ :,.]") text wrap with Not_found -> wrap in
-        spaces prev_indent
-        ^ Str.string_before text (if (String.get text last_space)=' ' then last_space else last_space+1)
-        ^ "\n" ^ format (indent+prev_indent) (Str.string_after text (last_space+1))
-  in if width != 0 then format 0 text else text;;
+    | _ when from_pos >= wrap_pos -> ""
+    | _ when wrap_pos = String.length text ->
+      Printf.fprintf stdout "newline=%d,indent=%d,prev_indent=%d,len=%d, '%s'\n"
+      wrap_pos cur_indent prev_indent (String.length text) (String.sub text from_pos (wrap_pos-from_pos));
+      spaces prev_indent ^ String.sub text from_pos (wrap_pos-from_pos)
+    | _ when String.get text wrap_pos = '\n' -> (* wrap at a \n symbol *)
+      spaces prev_indent ^ String.sub text from_pos (wrap_pos-from_pos)
+        ^"\n"^ format 0 (wrap_pos+1)
+    | _ -> (* wrap at any point because width is too large *)
+      let last_space = try Str.search_backward (Str.regexp "\\( \\|: \\|, \\|. \\)") text wrap_pos with Not_found -> wrap_pos in
+      let last_space_end = last_space + if (String.get text last_space)=' ' then 0 else 1 in
+        spaces prev_indent ^ String.sub text from_pos (last_space_end-from_pos)
+        ^ "\n" ^ format (cur_indent+prev_indent) (last_space_end+1)
+  in if width != 0 then format 0 0 else text;;
 
-let rm_trailing_whitespace text = Str.global_replace (Str.regexp "[\n ]*$") "" text
-
-(* NOTE: all msg should be finished by a trailing newline (\n) *)
 let rec string_of_msgs ?(width=78) ?(color=false) ?(fmt="%l:%c: %t: %m") (messages:t) =
+  let color_backquote text = let colorize str = "\x1b[33m" ^ str ^ "\x1b[0m" in
+    Str.global_substitute (Str.regexp "`\\([^`]\\)`") (fun s -> "`"^ colorize (Str.matched_group 1 s) ^"`") text in
+  let color_quoted text = let colorize str = "\x1b[33m" ^ str ^ "\x1b[0m" in
+    Str.global_substitute (Str.regexp "'\\([^']+\\)'") (fun s -> "'"^ colorize (Str.matched_group 1 s) ^"'") text in
+  let color_code text = let colorize str = "\x1b[37m" ^ str ^ "\x1b[0m" in
+    Str.global_substitute (Str.regexp "^\\(    +.*\\)$") (fun s -> colorize (Str.matched_group 1 s)) text in
+  let color_type text = let colorize str = match str with
+    | "warning" -> (* yellow bold *) "\x1b[33m\x1b[1m" ^str^ "\x1b[0m"
+    | "error" ->   (* red bold    *) "\x1b[31m\x1b[1m" ^str^ "\x1b[0m"
+    | str -> str
+    in Str.substitute_first (Str.regexp "\\(error\\|warning\\)") (fun s -> colorize (Str.matched_group 1 s)) text in
+  let color_all text = if color then text |> color_code |> color_backquote |> color_quoted |> color_type else text in
   fold
     (fun (typ,_,msg,loc) acc ->
-      let msg = rm_trailing_whitespace msg in
-      (replace (all_placeholders loc typ color msg) fmt |> format_width width) ^ acc)
+      (replace (all_placeholders loc typ color msg) fmt
+        |> format_width color width |> color_all) ^ acc)
     messages ""
-
-(** Rules for good error or warning messages:
-    - use \n only when the line break is mandatory for understanding.
-      The text will be wrapped automatically.
-    - use indentations for pieces of code you want to show: the indentation
-      will be kept when wrapping as long as no \n is encountered.
-    - you must not end your message with a new line (the \n character).
-      This new line will be automatically removed anyway.
-*)
