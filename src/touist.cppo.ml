touist/touist

commit 1e042be94e50fa7f6c7e9055c2dfd96bb81e0226

touist: add a debug mode (#272)
* add an option (--debug) which enable the display of stacktrace
	on exception catched on exit

src/touist.cppo.ml
@@ -51,6 +51,7 @@ let equiv_file_path = ref ""
 let input_equiv = ref stdin
 let linter = ref false (* for displaying syntax errors (during parse and eval) *)
 let error_format = ref "%f: line %l, col %c-%C: %t: %m" (* display absolute position of error *)
+let debug = ref false
 let debug_syntax = ref false
 let debug_cnf = ref false
 let latex = ref false
@@ -98,6 +99,7 @@ let () =
       ));
     ("--version", Arg.Set version_asked, "Display version number");
     ("-", Arg.Set use_stdin,"reads from stdin instead of file");
+    ("--debug", Arg.Set debug, "Print information for debugging touist");
     ("--debug-syntax", Arg.Set debug_syntax, "Print information for debugging
     syntax errors given by parser.messages");
     ("--debug-cnf", Arg.Set debug_cnf,"Print step by step CNF transformation");
@@ -340,12 +342,13 @@ let () =
   exit_with OK
 
   with
-    | Fatal (Usage,msgs) -> (show_msgs_and_exit msgs CMD_USAGE)
-    | Fatal (Parse,msgs) -> (show_msgs_and_exit msgs TOUIST_SYNTAX)
-    | Fatal (Lex,msgs) -> (show_msgs_and_exit msgs TOUIST_SYNTAX)
-    | Fatal (Eval,msgs) -> (show_msgs_and_exit msgs TOUIST_SYNTAX)
-    | Fatal (Sat,msgs) -> (show_msgs_and_exit msgs TOUIST_SYNTAX)
-    | Fatal (Cnf,msgs) -> (show_msgs_and_exit msgs TOUIST_SYNTAX)
-    | Fatal (Prenex,msgs) -> (show_msgs_and_exit msgs TOUIST_SYNTAX)
+    | Fatal (during,msgs) ->
+      (show_msgs_and_exit msgs
+        (match during with
+          | Usage -> CMD_USAGE
+          | _ -> TOUIST_SYNTAX
+        )
+      );
+      if !debug then Printf.eprintf "Stacktrace:\n%s\n" (Printexc.get_backtrace ())
     | Sys_error err -> show_msgs_and_exit (of_list [(Error,Usage,err^"\n",None)]) CMD_USAGE
     | x -> raise x

commit e29a84d83914750386d782e2e974ba61dc4ce780

cli: removed the duplicate '\n' after each error message
That duplicate \n was introduced in #272.

src/touist.cppo.ml
@@ -326,11 +326,13 @@ let () =
   exit_with OK
 
   with
+    (* Warning: the [msg] already contains an ending '\n'. No need for adding
+       another ending newline after it. *)
     | Fatal msg ->
       if !debug then Printf.eprintf "Stacktrace:\n%s\n" (Printexc.get_backtrace ());
-      Printf.eprintf "%s\n" (Msgs.string_of_msg msg);
+      Printf.eprintf "%s" (Msgs.string_of_msg msg);
       exit_with (match msg with _,Usage,_,_ -> CMD_USAGE | _ -> TOUIST_SYNTAX)
     | Sys_error err ->
-      Printf.eprintf "%s\n" (Msgs.string_of_msg (Error,Usage,err^"\n",None));
+      Printf.eprintf "%s" (Msgs.string_of_msg (Error,Usage,err,None));
       exit_with CMD_USAGE
     | x -> raise x