849ce91c538a38255c90cb6836387f8d3fda3b99
Maël Valais <mael.valais@gmail.com>
2017-09-11 10:27:47 +0200

    api: added touistVersion to API doc

--- src/lib/touistVersion.cppo.ml
+++ src/lib/touistVersion.cppo.ml
@@ -1,7 +1,14 @@
-(* This is the version given in _oasis. These preprocessor macros are
-   computed by cppo (see Makefile) *)
+(** Version information for the TouIST library.
+
+    This file is computed by cppo (see Makefile).
+    - {!version} is the OPAM version (as seen in the _oasis file)
+    - {!has_git_tag} tells if the library has been built inside a git repo;
+      if it has, then {!git_tag} contains the latest git tag using
+      [git describe --tags] *)
+
 let version = VERSION
 let has_git_tag = HAS_GIT_TAG
 let git_tag = GIT_TAG
+
 let has_yices2 = HAS_YICES2
 let has_qbf = HAS_QBF
