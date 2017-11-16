d28a5ecb5bfd9eb4917dbfa79e9e4256ac3952dc
Maël Valais <mael.valais@gmail.com>
2017-11-16 23:57:47 +0100

    js: stuck with error 'unbound latex_ret_to_yojson'
    See https://github.com/ocaml-ppx/ppx_deriving_yojson/issues/58

--- src/js/touistjs.ml
+++ src/js/touistjs.ml
@@ -11,50 +11,53 @@ type loc = {
   start_abs: int;
   end_abs: int;
 }
-[@@deriving to_yojson]
+[@@deriving yojson]
 type msg = {
   level: string;
   message: string;
   loc: loc;
-} [@@deriving to_yojson]
+} [@@deriving yojson]
 
 type messages = {
   messages: msg list;
-} [@@deriving to_yojson]
+} [@@deriving yojson]
 
 type linter_ret =
   | Lint_Succ of messages
   | Lint_Error of messages
-[@@deriving to_yojson]
+[@@deriving yojson]
 
 type latex_ret =
   | Latex_Succ of string * messages
   | Latex_Error of messages
-[@@deriving to_yojson]
+[@@deriving yojson]
 
-let to_msg_list (msgs : Msgs.t) : msg list =
-  let open Msgs in
-  fold (fun (typ,_,msg,pos) acc ->
-    let l,c,s,e = get_loc pos in
+
+(* WARNING: because 'warnings' are outputed to stderr directly,
+   we only get (for now) the latest error. *)
+let to_msg (msg: TouistErr.msg) : msg =
+  let open TouistErr in begin
+    let typ,_,msg,pos = msg in
+    let l,c,s,e = (match pos with None -> 0,0,0,0 | Some l -> get_loc l)  in
     let loc = {line=l;col=c;start_abs=s;end_abs=e} in
-    {level = string_of_type typ; message=msg; loc=loc}::acc)
-    msgs []
+    {level = string_of_type typ; message=msg; loc=loc}
+  end
 
 let _ =
   Js.export_all (object%js
     method linter (txt:Js.js_string Js.t) : Js.js_string Js.t =
       let ret =
-      try let _,msgs = Parse.parse_sat (Js.to_string txt) |> Eval.eval ~onlychecktypes:true in
-        Lint_Succ ({messages=to_msg_list !msgs})
-      with Msgs.Fatal msgs ->
-        Lint_Error ({messages=to_msg_list msgs})
+      try let _ = TouistParse.parse_sat (Js.to_string txt) |> TouistEval.eval ~onlychecktypes:true in
+        Lint_Succ ({messages=[]})
+      with TouistErr.TouistFatal msgs ->
+        Lint_Error ({messages=[to_msg msgs]})
       in linter_ret_to_yojson ret |> Yojson.Safe.to_string ~std:true |> Js.string
 
     method latex (txt:Js.js_string Js.t) : Js.js_string Js.t =
-      let ret = 
-      try let ast,msgs = Parse.parse_sat (Js.to_string txt) in let latex = Latex.latex_of_ast ast in
-        Latex_Succ (latex, {messages=to_msg_list !msgs})
-      with Msgs.Fatal msgs ->
-        Latex_Error ({messages=to_msg_list msgs})
+      let ret =
+      try let ast = TouistParse.parse_sat (Js.to_string txt) in let latex = TouistLatex.latex_of_ast ast in
+        Latex_Succ (latex ~full:false, {messages=[]})
+      with TouistErr.TouistFatal msgs ->
+        Latex_Error ({messages=[to_msg msgs]})
       in latex_ret_to_yojson ret |> Yojson.Safe.to_string ~std:true |> Js.string
   end)
