7b17fa3de33e1c4d9419110b3b5c7cc54858b54f
Maël Valais <mael.valais@gmail.com>
2017-11-16 21:21:14 +0100

    appveyor: for now 'qbf' has a 'no win32' flag, I cannot install from opam

--- ci/appveyor-script.sh
+++ ci/appveyor-script.sh
@@ -12,7 +12,8 @@ bash opam32/install.sh
 opam init -y -a mingw https://github.com/fdopen/opam-repository-mingw.git --comp 4.03.0+mingw32c --switch 4.03.0+mingw32c
 eval `opam config env`
 opam update
-opam install -y ocamlfind menhir minisat cppo_ocamlbuild qbf ounit zarith depext-cygwinports jbuilder
+opam install -y ocamlfind menhir minisat cppo_ocamlbuild ounit zarith depext-cygwinports jbuilder
+opam pin add -y qbf https://github.com/c-cube/ocaml-qbf.git
 
 if ! ocamlfind query yices2; then
     # We want a static libgmp.a. The mingw64-i686-gmp version only contains a
