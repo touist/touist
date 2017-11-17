764509c6acd6d5994cfcdd4138d30dcabfccccea
Maël Valais <mael.valais@gmail.com>
2017-11-17 10:39:10 +0100

    js: add comments in touistjs_test

---
+++ src/js/touistjs_test.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+// To test it: go to where is touistjs_test.js and
+//     node ./touistjs_test.js
+
+var t = require("../../_build/default/src/js/touistjs.bc.js");
+
+console.log(t.latex("bigand $i in [1..2]: p($i end"))
