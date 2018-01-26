cc5c8c4fe09675745ad97dcf934f54346e5f660b
Maël Valais <mael.valais@gmail.com>
2018-01-26 10:22:24 +0100

    prepare_version: use vars for colors & comply to shellcheck
    [skip ci]

--- prepare_version.sh
+++ prepare_version.sh
@@ -10,10 +10,16 @@
 # So I wrote this script to ease the process of 'Bumping' the version
 # number.
 #
-
-I='\033[94m+\033[0m' # blue
-Q='\033[93m?\033[0m' # yellow
-E='\033[91m!\033[0m' # red
+gray='\033[90m'
+red='\033[91m'
+green='\033[92m'
+yel='\033[93m'
+blu='\033[94m'
+purp='\033[95m'
+end='\033[0m'
+I="$blu+$end" # blue
+Q="$yel?$end" # yellow
+E="$red!$end" # red
 
 # Usage: parse_changelog <changelog_file_name> <version>
 # The changelog entry that starts with 'version' will be printed to stdout.
@@ -21,7 +27,7 @@ function parse_changelog() {
     CHANGELOG=$1
     VERSION=$2
     in_block=false
-    while IFS= read line; do
+    while IFS= read -r line; do
         if echo "$line" | grep "^$VERSION" ; then
             echo
             echo "$line"
@@ -32,7 +38,7 @@ function parse_changelog() {
             fi
             echo "$line"
         fi
-    done < $CHANGELOG
+    done < "$CHANGELOG"
 }
 
 while [ -n "$1" ]; do
@@ -63,9 +69,9 @@ EOF
                 echo -e "${E} Error, '$VERSION' should be of the form 'vX.Y.Z[+betaX]'"
                 exit 1
             else
-                SUFFIX=$(echo "$VERSION" | sed 's/^v[0-9]\.[0-9]\.[0-9]\(\+beta[0-9]*\)*$/\1/')
-                VERSION=$(echo "$VERSION" | sed 's/^\(v[0-9]\.[0-9]\.[0-9]\)\(\+beta[0-9]*\)*$/\1/')
-                echo -e "${I} Version:\033[93m${VERSION}\033[0m\033[92m${SUFFIX}\033[0m"
+                SUFFIX=${VERSION/v[0-9].[0-9].[0-9]/}
+                VERSION=${VERSION/$SUFFIX/}
+                echo -e "${I} Version:${yel}${VERSION}${end}${green}${SUFFIX}${end}"
             fi
             ;;
         *)
@@ -94,55 +100,55 @@ if [ "x$VERSION" = x ]; then
         echo -e "${E} Please give a version number as argument."
         exit 1
     fi
-    echo -e "${Q} Should we use \033[92m${VERSION}\033[0m from CHANGELOG? [Y/n]"
-    read answerAutoNumber
+    echo -e "${Q} Should we use ${green}${VERSION}${end} from CHANGELOG? [Y/n]"
+    read -r answerAutoNumber
     case $answerAutoNumber in n)
         exit 1 ;;
     esac
 fi
 
 # At this point, we know what VERSION we are going to use.
-echo -e "${I} Parsing the changelog for version '\033[92m${VERSION}\033[0m'..."
+echo -e "${I} Parsing the changelog for version '${green}${VERSION}${end}'..."
 parse_changelog CHANGELOG "$VERSION" > tag_content
 # In case the date has already been added to CHANGELOG, remove it for the tag
 sed 's/\(v[0-9]\.[0-9]\.[0-9][^ ]*\).*/\1/' tag_content > temp && mv temp tag_content
 
 # For the opam version, remove the leading 'v'.
 OPAM_VERSION=$(echo "$VERSION$SUFFIX" | cut -c 2-)
-PREV_OPAM_VERSION=$(grep "^version:" touist.opam | sed "s/^version: *\"\(.*\)\"$/\1/")
-echo -e "${I} Changing the 'version:' field in 'touist.opam' from \033[95m${PREV_OPAM_VERSION}\033[0m to \033[92m${OPAM_VERSION}\033[0m"
-sed "s/^\(version: *\)\"[0-9][0-9\.]*[^ ]*\"$/\1\"${OPAM_VERSION}\"/" touist.opam > a && mv a touist.opam
-echo -e "\033[90m$(git diff -U0 touist.opam | grep "^\(\+\|-\)[^+-]")\033[0m"
+PREV_OPAM_VERSION=$(grep "^version:" touist.opam | sed 's/^version: *\"\(.*\)\"$/\1/')
+echo -e "${I} Changing the 'version:' field in 'touist.opam' from ${purp}${PREV_OPAM_VERSION}${end} to ${green}${OPAM_VERSION}${end}"
+sed "s/^\\(version: *\\)\"[0-9][0-9\\.]*[^ ]*\"$/\\1\"${OPAM_VERSION}\"/" touist.opam > a && mv a touist.opam
+echo -e "${gray}$(git diff -U0 touist.opam | grep '^\(\+\|-\)[^+-]')${end}"
 
 if [ -z "$SUFFIX" ]; then # If 'beta', then no date change
-    echo -e "${I} Change version line in CHANGELOG from \033[95m${OPAM_VERSION}\033[0m to \033[92m${OPAM_VERSION} ($(date -I))\033[0m:"
-    sed "s/^\(${VERSION//./\\.}[^ ]*\).*$/\1 (`date -I`)/" CHANGELOG > a && mv a CHANGELOG
-    echo -e "\033[90m$(git diff -U0 CHANGELOG | grep "^\(\+\|-\)[^+-]")\033[0m"
+    echo -e "${I} Change version line in CHANGELOG from ${purp}${OPAM_VERSION}${end} to ${green}${OPAM_VERSION} ($(date -I))${end}:"
+    sed "s/^\\(${VERSION//./\\.}[^ ]*\\).*$/\\1 ($(date -I))/" CHANGELOG > a && mv a CHANGELOG
+    echo -e "${gray}$(git diff -U0 CHANGELOG | grep '^\(\+\|-\)[^+-]')${end}"
 else
-    echo -e "${I} Warning: you gave a suffix \033[91m${SUFFIX}\033[0m so we don't add the date to CHANGELOG"
+    echo -e "${I} Warning: you gave a suffix ${red}${SUFFIX}${end} so we don't add the date to CHANGELOG"
     echo -e "${I} Instead, use the suffix to the tag"
-    sed "s/^\(${VERSION//./\\.}[^ ]*\).*$/\1${SUFFIX}/g" tag_content > a && mv a tag_content
+    sed "s/^\\(${VERSION//./\\.}[^ ]*\\).*$/\\1${SUFFIX}/g" tag_content > a && mv a tag_content
 fi
 echo -e "${I} Changes done."
-echo -e "${Q} Do you want to commit using message '\033[92mBump to $VERSION$SUFFIX\033[0m'? [Y/n]"
-read answerCommit
-echo $answerCommit
-case $answerCommit in
+echo -e "${Q} Do you want to commit using message '${green}Bump to $VERSION$SUFFIX${end}'? [Y/n]"
+read -r answerCommit
+echo "$answerCommit"
+case "$answerCommit" in
     y | Y)
-        echo -e "${I} Running 'git add *.opam *.descr CHANGELOG'\033[0m"
-        git add *.opam *.descr CHANGELOG
-        echo -e "${I} Commiting with message '\033[92mBump to $VERSION$SUFFIX\033[0m'"
+        echo -e "${I} Running 'git add *.opam *.descr CHANGELOG'${end}"
+        git add ./*.opam ./*.descr CHANGELOG
+        echo -e "${I} Commiting with message '${green}Bump to $VERSION$SUFFIX${end}'"
         git commit -m "Bump to $VERSION$SUFFIX"
         ;;
     *) echo -e "${I} Doing nothing.";;
 esac
 
-echo -e "${Q} Do you want to create a signed tag '\033[92mBump to $VERSION$SUFFIX\033[0m' using the following message (u for unsigned tag; y for signed tag): [Y/n/u]"
-while IFS= read line; do
-    echo -e "\033[90m${line}\033[0m"
+echo -e "${Q} Do you want to create a signed tag '${green}Bump to $VERSION$SUFFIX${end}' using the following message:"
+while IFS= read -r line; do
+    echo -e "\${gray}${line}${end}"
 done < tag_content
-
-read ans
+echo -en "${Q} (u for unsigned tag; y for signed tag): [Y/n/u] "
+read -r ans
 case "$ans" in
     y|Y|u)
         echo -e "${I} Creating the tag..."
