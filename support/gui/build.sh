bf0f9ba1c97060f10caee2739a2fc7215dacbd8c
Maël Valais <mael.valais@gmail.com>
2018-02-17 12:18:34 +0100

    add build.sh for turning a jar into a module

---
+++ support/gui/build.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+set -e
+JARPATH=$1
+MOD=$2
+ROOT_DIR=$PWD
+
+# copy original jar into place
+mkdir -p "$ROOT_DIR/out"
+cp "$ROOT_DIR/$JARPATH" "$ROOT_DIR/out/$MOD.jar"
+
+# extract original jar
+rm -rf build/classes-extracted
+mkdir build/classes-extracted
+cd build/classes-extracted
+jar xf "$ROOT_DIR/$JARPATH"
+
+# generate module-info.java
+cd "$ROOT_DIR"
+jdeps --generate-module-info work "$1"
+# compile module-info.java
+cd "$ROOT_DIR/work/$MOD"
+javac -p "$MOD" -d "$ROOT_DIR"/build/classes-extracted module-info.java
+
+# update output jar
+jar uf "$ROOT_DIR/out/$MOD.jar" -C "$ROOT_DIR/build/classes-extracted" module-info.class
+
+cd "$ROOT_DIR"
+echo "Ready."
+echo "now, do:"
+echo "/Library/Java/JavaVirtualMachines/jdk-9.0.4.jdk/Contents/Home/bin/jlink --add-modules touist --limit-modules touist --launcher touist=touist/touist.TouIST --output dist --module-path out/touist.jar:/Library/Java/JavaVirtualMachines/jdk-9.0.4.jdk/Contents/Home/jmods --limit-modules touist --no-man-pages --no-header-files --strip-debug --compress 2"
