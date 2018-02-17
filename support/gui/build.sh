6fe11194ebd81c2ce17737b164ea66b4b661c7cb
Maël Valais <mael.valais@gmail.com>
2018-02-17 12:45:38 +0100

    build.sh: always generate in build/ prefix

--- support/gui/build.sh
+++ support/gui/build.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/env bash
 set -e
-JARPATH=$1
+JARPATH=$1 # relative to current dir
 MOD=$2
 ROOT_DIR=$PWD
 
 # copy original jar into place
-mkdir -p "$ROOT_DIR/out"
-cp "$ROOT_DIR/$JARPATH" "$ROOT_DIR/out/$MOD.jar"
+mkdir -p "$ROOT_DIR/build/out"
+cp "$ROOT_DIR/$JARPATH" "$ROOT_DIR/build/out/$MOD.jar"
 
 # extract original jar
 rm -rf build/classes-extracted
@@ -16,15 +16,15 @@ jar xf "$ROOT_DIR/$JARPATH"
 
 # generate module-info.java
 cd "$ROOT_DIR"
-jdeps --generate-module-info work "$1"
+jdeps --generate-module-info build/work "$1"
 # compile module-info.java
-cd "$ROOT_DIR/work/$MOD"
+cd "$ROOT_DIR/build/work/$MOD"
 javac -p "$MOD" -d "$ROOT_DIR"/build/classes-extracted module-info.java
 
 # update output jar
-jar uf "$ROOT_DIR/out/$MOD.jar" -C "$ROOT_DIR/build/classes-extracted" module-info.class
+jar uf "$ROOT_DIR/build/out/$MOD.jar" -C "$ROOT_DIR/build/classes-extracted" module-info.class
 
 cd "$ROOT_DIR"
 echo "Ready."
 echo "now, do:"
-echo "/Library/Java/JavaVirtualMachines/jdk-9.0.4.jdk/Contents/Home/bin/jlink --add-modules touist --limit-modules touist --launcher touist=touist/touist.TouIST --output dist --module-path out/touist.jar:/Library/Java/JavaVirtualMachines/jdk-9.0.4.jdk/Contents/Home/jmods --limit-modules touist --no-man-pages --no-header-files --strip-debug --compress 2"
+echo "/Library/Java/JavaVirtualMachines/jdk-9.0.4.jdk/Contents/Home/bin/jlink --add-modules touist --limit-modules touist --launcher touist=touist/touist.TouIST --output dist --module-path build/out/touist.jar:/Library/Java/JavaVirtualMachines/jdk-9.0.4.jdk/Contents/Home/jmods --limit-modules touist --no-man-pages --no-header-files --strip-debug --compress 2"
