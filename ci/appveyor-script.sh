5877180e2321ce13e3a901b39dddfb7c7f5a5ced
Maël Valais <mael.valais@gmail.com>
2018-12-27 22:21:34 +0100

    ci: fix for opam2, add touist-cli.tar.gz to releases, use ocaml 4.07.1

--- ci/appveyor-script.sh
+++ ci/appveyor-script.sh
@@ -7,30 +7,30 @@ set -x
 TOUIST_BUILD_DIR=$PWD
 
 cd $HOME
-curl -L https://github.com/fdopen/opam-repository-mingw/releases/download/0.0.0.1/opam32.tar.xz | tar xJ
+curl -L https://github.com/fdopen/opam-repository-mingw/releases/download/0.0.0.2/opam32.tar.xz | tar xJ
 bash opam32/install.sh
-opam init -y -a mingw https://github.com/fdopen/opam-repository-mingw.git --comp 4.03.0+mingw32c --switch 4.03.0+mingw32c
-eval `opam config env`
+opam init -y -a default "https://github.com/fdopen/opam-repository-mingw.git#opam2" -c "ocaml-variants.4.07.1+mingw32c" --disable-sandboxing
+eval $(opam config env)
 opam update
 opam install -y ocamlfind menhir minisat ounit zarith depext-cygwinports jbuilder re cmdliner
 opam pin add -y qbf https://github.com/c-cube/ocaml-qbf.git
 
 if ! ocamlfind query yices2; then
-    # We want a static libgmp.a. The mingw64-i686-gmp version only contains a
-    # shared-only version of libgmp. I managed to upload a static version of
-    # libgmp.a in the ocamlyices2's releases. We simply override the installed
-    # shared libgmp.a with our static libgmp.a.
-    curl -L https://github.com/maelvalais/ocamlyices2/releases/download/v0.0.3/gmp-6.1.2-static-i686-w64-mingw32.tar.gz | tar xz
+	# We want a static libgmp.a. The mingw64-i686-gmp version only contains a
+	# shared-only version of libgmp. I managed to upload a static version of
+	# libgmp.a in the ocamlyices2's releases. We simply override the installed
+	# shared libgmp.a with our static libgmp.a.
+	curl -L https://github.com/maelvalais/ocamlyices2/releases/download/v0.0.3/gmp-6.1.2-static-i686-w64-mingw32.tar.gz | tar xz
 
-    # Now, download the ocamlyices2 tarball and build the ocaml library
-    curl -L https://github.com/maelvalais/ocamlyices2/archive/v0.0.3.tar.gz | tar xz
-    cd ocamlyices2-0.0.3
-    ./configure --host=i686-w64-mingw32 --with-static-gmp=$HOME/gmp-6.1.2-static-i686-w64-mingw32/libgmp.a
-    make
-    make install
+	# Now, download the ocamlyices2 tarball and build the ocaml library
+	curl -L https://github.com/maelvalais/ocamlyices2/archive/v0.0.3.tar.gz | tar xz
+	cd ocamlyices2-0.0.3
+	./configure --host=i686-w64-mingw32 --with-static-gmp=$HOME/gmp-6.1.2-static-i686-w64-mingw32/libgmp.a
+	make
+	make install
 fi
 
-cd $TOUIST_BUILD_DIR
+cd "$TOUIST_BUILD_DIR"
 # Build touist
 jbuilder build
 
@@ -43,7 +43,6 @@ jbuilder install
 
 # 'jbuilder runtest' and 'jbuilder uninstall' are done in appveyor.yml
 
-
 # Build the actual TouIST.exe
 cd support/gui
 TERM=dumb ./gradlew createExeZip createJarZip
@@ -51,14 +50,23 @@ ARCH=windows-x86
 
 # Add $ARCH to the end of the .zip filename
 for f in $(find build/distributions -name "TouIST*"); do
-      mv $f $(echo $f | sed "s/\(.*\)\.zip$/\1-${ARCH}.zip/")
+	mv "$f" $(echo $f | sed "s/\(.*\)\.zip$/\1-${ARCH}.zip/")
 done
+
 ls build/distributions
-cd ../..
+cd "$TOUIST_BUILD_DIR"
 
 git status
-if ! git status 2> /dev/null | tail -n1 | grep "nothing.*clean"; then
-    echo 'STOP!!! The build ends in a dirty state; see the diff:'
-    git diff
-    exit 1
+if ! git status 2>/dev/null | tail -n1 | grep "nothing.*clean"; then
+	echo 'STOP!!! The build ends in a dirty state; see the diff:'
+	git diff
+	exit 1
 fi
+
+# Now that we are done with TouIST-v1.2.3-windows-x86, lets create the
+# touist-cli-v1.2.3-windows-x86.zip
+cd _build/default/src/
+mv ./*.exe touist.exe
+zip touist-cli-"$(git describe --tags)"-${ARCH}.zip touist.exe
+cd "$TOUIST_BUILD_DIR"
+mv _build/default/src/touist-cli* _build/
