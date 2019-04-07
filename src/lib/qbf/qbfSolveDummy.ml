29a55b19237984cab68c793214aa768366cc8124
Maël Valais <mael.valais@gmail.com>
2019-04-07 14:50:17 +0200

    build: move jbuild files to dune (using the awesome 'dune upgrade')

--- src/lib/qbf/qbfSolveDummy.ml
+++ src/lib/qbf/qbfSolveDummy.ml
@@ -1,5 +1,6 @@
 
 let ocamlqbf_of_ast _ = failwith "Qbf not available, recompile with Qbf"
 let qcnf_of_cnf _ = failwith "Qbf not available, recompile with Qbf"
-let solve ?_hidden _ = failwith "Qbf not available, recompile with Qbf"
+let solve ?(hidden=false) _ = failwith ("Qbf not available, recompile with Qbf"
+  ^ string_of_bool hidden)
 let enabled = false
--- src/lib/yices2/smtSolveDummy.ml
+++ src/lib/yices2/smtSolveDummy.ml
@@ -2,7 +2,7 @@
     it with Yices2. *)
 
 let ast_to_yices _ = failwith "Yices2 not available, recompile with Yices2"
-let string_of_model ?_value_sep _ _ = failwith "Yices2 not available, recompile with Yices2"
+let string_of_model ?(value_sep="\n") _ _ = failwith ("Yices2 not available, recompile with Yices2" ^ value_sep)
 let solve _ _ = failwith "Yices2 not available, recompile with Yices2"
 let logic_supported _ = failwith "Yices2 not available, recompile with Yices2"
 let enabled = false
