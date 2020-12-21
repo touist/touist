20bf283f852e97f5098ef5540b223a85751a2cf5
Maël Valais <mael.valais@gmail.com>
2020-12-21 20:04:22 +0100

    chore(prepare_version): shfmt-ify

--- prepare_version.sh
+++ prepare_version.sh
@@ -28,7 +28,7 @@ function parse_changelog() {
     VERSION=$2
     in_block=false
     while IFS= read -r line; do
-        if echo "$line" | grep "^$VERSION" ; then
+        if echo "$line" | grep "^$VERSION"; then
             echo
             echo "$line"
             in_block=true
@@ -38,13 +38,13 @@ function parse_changelog() {
             fi
             echo "$line"
         fi
-    done < "$CHANGELOG"
+    done <"$CHANGELOG"
 }
 
 while [ -n "$1" ]; do
     case $1 in
-        --help | -h)
-            cat <<EOF
+    --help | -h)
+        cat <<EOF
 Usage: $0 [-h | --help] [vX.Y.Z][+betaX]
 
 If no vesion number vX.Y.Z is given, the script will pick the first version
@@ -60,25 +60,25 @@ This script will help you to successively:
 Using '+betaX', notes:
 - the changelog date won't be added as it is not a 'final' release
 EOF
-            exit 0
-            ;;
-        v*)
-            VERSION=$1
-            # Check that it has the right format.
-            if ! echo "$VERSION" |  grep '^v[0-9]\.[0-9]\.[0-9]\(\+beta[0-9]*\)*$'>/dev/null; then
-                echo -e "${E} Error, '$VERSION' should be of the form 'vX.Y.Z[+betaX]'"
-                exit 1
-            else
-                SUFFIX=${VERSION/v[0-9].[0-9].[0-9]/}
-                VERSION=${VERSION/$SUFFIX/}
-                echo -e "${I} Version:${yel}${VERSION}${end}${green}${SUFFIX}${end}"
-            fi
-            ;;
-        *)
-            echo "Unknown argument: '$1'"
-            $0 --help
+        exit 0
+        ;;
+    v*)
+        VERSION=$1
+        # Check that it has the right format.
+        if ! echo "$VERSION" | grep '^v[0-9]\.[0-9]\.[0-9]\(\+beta[0-9]*\)*$' >/dev/null; then
+            echo -e "${E} Error, '$VERSION' should be of the form 'vX.Y.Z[+betaX]'"
             exit 1
-            ;;
+        else
+            SUFFIX=${VERSION/v[0-9].[0-9].[0-9]/}
+            VERSION=${VERSION/$SUFFIX/}
+            echo -e "${I} Version:${yel}${VERSION}${end}${green}${SUFFIX}${end}"
+        fi
+        ;;
+    *)
+        echo "Unknown argument: '$1'"
+        $0 --help
+        exit 1
+        ;;
     esac
     shift
 done
@@ -103,60 +103,64 @@ if [ "x$VERSION" = x ]; then
     echo -e "${Q} Should we use ${green}${VERSION}${end} from CHANGELOG? [Y/n]"
     read -r answerAutoNumber
     case $answerAutoNumber in n)
-        exit 1 ;;
+        exit 1
+        ;;
     esac
 fi
 
 # At this point, we know what VERSION we are going to use.
 echo -e "${I} Parsing the changelog for version '${green}${VERSION}${end}'..."
-parse_changelog CHANGELOG "$VERSION" > tag_content
+parse_changelog CHANGELOG "$VERSION" >tag_content
 # In case the date has already been added to CHANGELOG, remove it for the tag
-sed 's/\(v[0-9]\.[0-9]\.[0-9][^ ]*\).*/\1/' tag_content > temp && mv temp tag_content
+sed 's/\(v[0-9]\.[0-9]\.[0-9][^ ]*\).*/\1/' tag_content >temp && mv temp tag_content
 
 # For the opam version, remove the leading 'v'.
 OPAM_VERSION=$(echo "$VERSION$SUFFIX" | cut -c 2-)
 PREV_OPAM_VERSION=$(grep "^version:" touist.opam | sed 's/^version: *\"\(.*\)\"$/\1/')
 echo -e "${I} Changing the 'version:' field in 'touist.opam' from ${purp}${PREV_OPAM_VERSION}${end} to ${green}${OPAM_VERSION}${end}"
-sed "s/^\\(version: *\\)\"[0-9][0-9\\.]*[^ ]*\"$/\\1\"${OPAM_VERSION}\"/" touist.opam > a && mv a touist.opam
+sed "s/^\\(version: *\\)\"[0-9][0-9\\.]*[^ ]*\"$/\\1\"${OPAM_VERSION}\"/" touist.opam >a && mv a touist.opam
 echo -e "${gray}$(git diff -U0 touist.opam | grep '^\(\+\|-\)[^+-]')${end}"
 
 if [ -z "$SUFFIX" ]; then # If 'beta', then no date change
     echo -e "${I} Change version line in CHANGELOG from ${purp}${OPAM_VERSION}${end} to ${green}${OPAM_VERSION} ($(date -I))${end}:"
-    sed "s/^\\(${VERSION//./\\.}[^ ]*\\).*$/\\1 ($(date -I))/" CHANGELOG > a && mv a CHANGELOG
+    sed "s/^\\(${VERSION//./\\.}[^ ]*\\).*$/\\1 ($(date -I))/" CHANGELOG >a && mv a CHANGELOG
     echo -e "${gray}$(git diff -U0 CHANGELOG | grep '^\(\+\|-\)[^+-]')${end}"
 else
     echo -e "${I} Warning: you gave a suffix ${red}${SUFFIX}${end} so we don't add the date to CHANGELOG"
     echo -e "${I} Instead, use the suffix to the tag"
-    sed "s/^\\(${VERSION//./\\.}[^ ]*\\).*$/\\1${SUFFIX}/g" tag_content > a && mv a tag_content
+    sed "s/^\\(${VERSION//./\\.}[^ ]*\\).*$/\\1${SUFFIX}/g" tag_content >a && mv a tag_content
 fi
 echo -e "${I} Changes done."
 echo -e "${Q} Do you want to commit using message '${green}Bump to $VERSION$SUFFIX${end}'? [Y/n]"
 read -r answerCommit
 echo "$answerCommit"
 case "$answerCommit" in
-    y | Y)
-        echo -e "${I} Running 'git add *.opam *.descr CHANGELOG'${end}"
-        git add ./*.opam ./*.descr CHANGELOG
-        echo -e "${I} Commiting with message '${green}Bump to $VERSION$SUFFIX${end}'"
-        git commit -m "Bump to $VERSION$SUFFIX"
-        ;;
-    *) echo -e "${I} Doing nothing.";;
+y | Y)
+    echo -e "${I} Running 'git add *.opam *.descr CHANGELOG'${end}"
+    git add ./*.opam ./*.descr CHANGELOG
+    echo -e "${I} Commiting with message '${green}Bump to $VERSION$SUFFIX${end}'"
+    git commit -m "Bump to $VERSION$SUFFIX"
+    ;;
+*) echo -e "${I} Doing nothing." ;;
 esac
 
 echo -e "${Q} Do you want to create a signed tag '${green}Bump to $VERSION$SUFFIX${end}' using the following message:"
 while IFS= read -r line; do
     echo -e "${gray}${line}${end}"
-done < tag_content
+done <tag_content
 echo -en "${Q} (u for unsigned tag; y for signed tag): [Y/n/u] "
 read -r ans
 case "$ans" in
-    y|Y|u)
-        echo -e "${I} Creating the tag..."
-        SIGN=-s
-        if [ "$ans" = "u" ]; then SIGN=; echo -e "${I} (unsigned tag)"; fi
-        git tag $SIGN "$VERSION$SUFFIX" --file tag_content
-        ;;
-    *) echo -e "${I} No tag created.";;
+y | Y | u)
+    echo -e "${I} Creating the tag..."
+    SIGN=-s
+    if [ "$ans" = "u" ]; then
+        SIGN=
+        echo -e "${I} (unsigned tag)"
+    fi
+    git tag $SIGN "$VERSION$SUFFIX" --file tag_content
+    ;;
+*) echo -e "${I} No tag created." ;;
 esac
 rm tag_content
 
